55e9c7619d4ea54835b5a9490ae4c09a2b4292c5
Katekko <joaovixgon@gmail.com>
2021-09-27 14:11:04 -0300

    Creating search screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:base_interview/search.screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,19 +13,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: HomeScreen(),
+      routes: {
+        '/': (_) => HomeScreen(),
+        '/search': (_) => SearchScreen(),
+      },
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Quest-2')),
       body: Center(
-        child: Text('Base Interview'),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/search');
+          },
+          child: const Text('Navegar para tela de pesquisa'),
+        ),
       ),
     );
   }
