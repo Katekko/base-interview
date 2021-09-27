717e8e27879a8064352447b35b48d352fab4405b
Katekko <joaovixgon@gmail.com>
2021-09-27 16:07:39 -0300

    Criando base para questao 3

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,52 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final cepController = TextEditingController();
+  final logradouroController = TextEditingController();
+  final bairroController = TextEditingController();
+  final cidadeController = TextEditingController();
+  final estadoController = TextEditingController();
+
+  Future<void> searchCep() async {}
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Base Interview'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Questão 3')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: cepController,
+                decoration: const InputDecoration(label: Text('CEP')),
+              ),
+              TextField(
+                controller: logradouroController,
+                decoration: const InputDecoration(label: Text('Logradouro')),
+              ),
+              TextField(
+                controller: bairroController,
+                decoration: const InputDecoration(label: Text('Bairro')),
+              ),
+              TextField(
+                controller: cidadeController,
+                decoration: const InputDecoration(label: Text('Cidade')),
+              ),
+              TextField(
+                controller: estadoController,
+                decoration: const InputDecoration(label: Text('Estado')),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
