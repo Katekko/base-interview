768e4d4908b8d308a080a5bf602f3bf41a7be6fa
Katekko <joaovixgon@gmail.com>
2021-09-27 14:02:01 -0300

    Login screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,19 +12,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final loginController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signIn() {}
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Base Interview'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Questão 1')),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: loginController,
+              decoration: const InputDecoration(label: Text('Login')),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(label: Text('Password')),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(onPressed: signIn, child: const Text('Sign in'))
+          ],
+        ),
       ),
     );
   }
